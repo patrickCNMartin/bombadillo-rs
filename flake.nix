beadbb84511613ac9c4888e39832b341b04daf8a
patrickCNMartin <pcnmartin@gmail.com>
2025-06-19 13:18:01 -0700

    Repro(nix): adding nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Bombadillo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    supportedSystems = [ "aarch64-darwin" "x86_64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f { inherit system; });
+  in {
+    devShells = forAllSystems ({ system }: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustc       # Rust compiler
+          pkgs.cargo       # Rust package manager/build tool
+        ];
+      };
+    });
+  };
+}
